1ae6613c1fe411cf9db789290794460f437bb3a6
udukunda1 <rumpatr048@gmail.com>
2025-01-14 00:07:30 +0200

    new com

--- src/components/topcontainer/TopContainer.js
+++ src/components/topcontainer/TopContainer.js
@@ -5,6 +5,8 @@ function TopContainer() {
     <div className='top-container'>
       <div className='top-container__accounts'>
         <p className='head'>Accounts</p>
+        <input type='text' />
+        <button>add</button>
       </div>
       <div className='top-container__categories'>
       <p className='head'>Categories</p>
