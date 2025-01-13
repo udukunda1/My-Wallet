7088bf2541806edef9cf38d655f1cd43dba49d76
udukunda1 <rumpatr048@gmail.com>
2025-01-14 00:23:31 +0200

    new com

--- src/components/topcontainer/TopContainer.js
+++ src/components/topcontainer/TopContainer.js
@@ -5,8 +5,12 @@ function TopContainer() {
     <div className='top-container'>
       <div className='top-container__accounts'>
         <p className='head'>Accounts</p>
-        <input type='text' />
+        <input type='text' placeholder='enter' />
         <button>+</button>
+        <div>
+            <p className='account-name'>MOMO</p>
+            <p className='acount-balance'>RWF 600</p>
+        </div>
       </div>
       <div className='top-container__categories'>
       <p className='head'>Categories</p>
