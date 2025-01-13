6a211f24a3a3748c588f6ba2cf2171429eabb997
udukunda1 <rumpatr048@gmail.com>
2025-01-13 23:40:05 +0200

    new com

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TopContainer from './components/topcontainer/TopContainer';
 
 function App() {
   return (
     <div >
      <p>MY Wallet</p>
+     <TopContainer />
     </div>
   );
 }
---
+++ src/components/topcontainer/TopContainer.js
@@ -0,0 +1,13 @@
+import './TopContainer.css';
+
+function TopContainer() {
+  return (
+    <div className='top-container'>
+      <div className='top-container__accounts'></div>
+      <div className='top-container__categories'></div>
+      <div className='top-container__budget-settings'></div>
+     </div>
+  )
+}
+
+export default TopContainer
