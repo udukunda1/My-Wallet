a600fb5989c96986f85289e4a508a02a84b6065e
udukunda1 <rumpatr048@gmail.com>
2025-01-14 00:31:38 +0200

    new com

--- src/components/topcontainer/TopContainer.js
+++ src/components/topcontainer/TopContainer.js
@@ -5,19 +5,21 @@ function TopContainer() {
     <div className='top-container'>
       <div className='top-container__accounts'>
         <p className='head'>Accounts</p>
-        <input type='text' placeholder='enter' />
+        <input type='text' placeholder='New account name' />
         <button>+</button>
         <div>
             <p className='account-name'>MOMO</p>
-            <p className='acount-balance'>RWF 600</p>
+            <p className='acount-balance negative'>RWF -700</p>
         </div>
         <div>
             <p className='account-name'>MOMO</p>
-            <p className='acount-balance'>RWF 600</p>
+            <p className='acount-balance positive'>RWF 600</p>
         </div>
       </div>
       <div className='top-container__categories'>
       <p className='head'>Categories</p>
+      <input type='text' placeholder='New category name' />
+        <button>+</button>
       </div>
       <div className='top-container__budget-settings'>
       <p className='head'>Budget Settings</p>
